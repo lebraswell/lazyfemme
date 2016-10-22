6ef63f9767ea152564ff34ec67179046d0ce5bde
Lauren Braswell <lebraswell@Janets-iMac.local>
2016-10-22 12:33:27 -0700

    adventure model, routing

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022033929) do
+ActiveRecord::Schema.define(version: 20161022184736) do
 
   create_table "adventures", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.string   "picture"
+    t.string   "location"
+    t.date     "visit"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
